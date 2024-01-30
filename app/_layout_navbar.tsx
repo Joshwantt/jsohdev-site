96d7da232d1fa7d305fe04ade074f752f849bc61
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-01-30 17:59:22 +1000

    update dependencies and fix stuff

--- app/_layout_navbar.tsx
+++ app/_layout_navbar.tsx
@@ -1,46 +1,47 @@
+'use client'
+
 import Link from 'next/link';
 import { Nav } from './_util_navigation';
+
+function dropdownClicked(index) {
+
+}
 
 export default function Navbar() {
   return (
     <div className="bg-primary text-primary-content">
-      <div className="containerNav flex justify-between">
+      <div className='navbar containerNav'>
+      <div className="navbar-start px-12">
         <Link className="btn btn-ghost normal-case text-xl" href="/">
           [ home ]
         </Link>
-        <div key='dropdowns'>
+      </div>
+      <div className="navbar-end px-12">
+        <div className='menu menu-horizontal'>
           {Object.keys(Nav).map((navKey, navIndex) => {
             const header = Nav[navKey];
             if (header.items.length > 0) {
             return (
-              <div className='menu menu-horizontal'>
               <li key={navIndex}>
-                  <Link href={header.link}>
+                <details>
+                  <summary className='text-xl hover'>
                     {header.headerNav}
-                    <svg
-                      className="fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                    </svg>
-                  </Link>
+                  </summary>
                   <ul className="p-2 bg-primary">
                     {header.items.map((item, itemIndex) => (
-                      <li key={itemIndex} className='z-50'>
+                      <li key={itemIndex} className='text-xl'>
                         <Link href={item.href}>{item.nameNav}</Link>
                       </li>
                     ))}
                   </ul>
+                </details>
               </li>
-              </div>
             )
             }
           })}
         </div>
-      </div>
+    </div>
+    </div>
     </div>
   );
 }
