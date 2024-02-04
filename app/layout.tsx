cfe6547a74ca86063dd0644ce0d02322553e1939
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-04 19:13:56 +1000

    add site icon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,40 @@
 import React from "react"
 import './globals.css'
-import Navbar from './_layout_navbar'
-import Footer from "./_layout_footer"
+import Navbar from './_layout/navbar'
+import Footer from "./_layout/footer"
+import type { Metadata } from 'next'
+import Head from "next/head"
+
+
+
+export const metadata: Metadata = {
+  title: 'Jsoh Dev',
+  description: 'Jsoh Dev / Portfolio and personal projects',
+}
 
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>
-          <Navbar/>
-          <main>
-            {children}
-          </main>
-          <Footer/>
-        </body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <Head>
+          <link
+            rel="icon"
+            href="/icon?<generated>"
+            type="image/<generated>"
+            sizes="<generated>"
+          />
+      </Head>
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
