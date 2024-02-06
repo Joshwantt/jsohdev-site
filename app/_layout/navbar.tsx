40e55f4b2965298fef94b3e86a889a41d31d72b2
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-06 20:07:51 +1000

    add simple session aware navbar

--- app/_layout/navbar.tsx
+++ app/_layout/navbar.tsx
@@ -1,37 +1,30 @@
 import Link from 'next/link';
+import { getServerSession } from "next-auth/next"
+import { authOptions } from '../_util/auth/auth';
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getServerSession(authOptions);
+
   return (
     <div className="bg-primary text-primary-content">
       <div className='navbar containerNav'>
-      <div className="navbar-start px-12">
-        <Link className="btn btn-ghost normal-case text-xl" href="/">
-          [ home ]
-        </Link>
-      </div>
-      {/* <div className="navbar-end px-12">
-          {Object.keys(Nav).map((navKey, navIndex) => {
-            const header = Nav[navKey];
-            if (header.items.length > 0) {
-            return (
-              <div className="dropdown dropdown-hover">
-                <Link href={header.link}>
-                <div tabIndex={0} role="button" className="btn m-1 bg-primary btn-ghost normal-case text-xl">{header.headerNav}</div>
-                </Link>
-                <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-primary rounded-box w-52">
-                    {header.items.map((item, itemIndex) => (
-                      <li key={itemIndex} className='text-xl'>
-                        <Link href={item.href}>{item.nameNav}</Link>
-                      </li>
-                    ))}
-                  </ul>
-              </div>
+        <div className="navbar-start px-12">
+          <Link className="btn btn-ghost normal-case text-xl" href="/">
+            [ home ]
+          </Link>
+        </div>
+        <div className="navbar-end px-12">
+          {
+            !session ? (
+              <Link href='/api/auth/signin'>
+              <button className="btn">Sign In</button>
+              </Link>
+            ) : (
+              <pre>{'Welcome '+session.user.name}</pre>
             )
-            }
-          })}
-        </div> */}
+          }
+        </div>
+      </div>
     </div>
-    </div>
-
   );
 }
