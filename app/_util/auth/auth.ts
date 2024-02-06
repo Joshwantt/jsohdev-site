61c1dbf82928f40ea1c978c933e9bebee549cc71
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-06 20:04:48 +1000

    refactor authOptions into helper function

---
+++ app/_util/auth/auth.ts
@@ -0,0 +1,38 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+import { DynamoDBAdapter } from "@auth/dynamodb-adapter"
+import { DynamoDB } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
+import { Table } from "sst/node/table";
+
+const client = DynamoDBDocument.from(new DynamoDB(), { //maybeneed to be a new documentclient
+    marshallOptions: {
+        convertEmptyValues: true,
+        removeUndefinedValues: true,
+        convertClassInstanceToMap: true,
+    },
+})
+
+export const authOptions: NextAuthOptions = {
+    callbacks: {
+        async session({ session, token, user }) {
+          session.user.id = user.id
+          
+          return session
+        }
+      },
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+    adapter: DynamoDBAdapter(client, {
+        tableName: Table.UserTable.tableName,
+        partitionKey: "pk",
+        sortKey: "sk",
+        indexName: "GSI1",
+        indexPartitionKey: "GSI1PK",
+        indexSortKey: "GSI1SK"
+    }),
+}
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,7 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb"
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github"
-import { DynamoDBAdapter } from "@auth/dynamodb-adapter"
-import { Table } from "sst/node/table";
+import { authOptions } from "../../../_util/auth/auth";
 
-const client = DynamoDBDocument.from(new DynamoDB(), { //maybeneed to be a new documentclient
-  marshallOptions: {
-    convertEmptyValues: true,
-    removeUndefinedValues: true,
-    convertClassInstanceToMap: true,
-  },
-})
-
-const handler = NextAuth({
-  providers: [
-    GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-      }),
-  ],
-  adapter: DynamoDBAdapter(client, {
-    tableName: Table.UserTable.tableName,
-    partitionKey: "pk",
-    sortKey: "sk",
-    indexName: "GSI1",
-    indexPartitionKey: "GSI1PK",
-    indexSortKey: "GSI1SK"
-  }),
-});
+const handler = NextAuth(authOptions);
 
 
 export { handler as GET, handler as POST }
