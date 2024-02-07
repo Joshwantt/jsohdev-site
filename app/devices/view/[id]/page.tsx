4d0f2025f45af69d4835b6fd28735da437980724
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-07 21:49:15 +1000

    new page route placeholders

---
+++ app/devices/view/[id]/page.tsx
@@ -0,0 +1,9 @@
+import ContentSection from "../../../_components/contentSection"
+
+export default function ViewID() {
+  return (
+    <div id="home" className="text-base-content">
+      View ID
+    </div>
+  );
+}
---
+++ app/devices/view/all/page.tsx
@@ -0,0 +1,9 @@
+import ContentSection from "../../../_components/contentSection"
+
+export default function ViewAll() {
+  return (
+    <div id="home" className="text-base-content">
+      all page placeholder
+    </div>
+  );
+}
