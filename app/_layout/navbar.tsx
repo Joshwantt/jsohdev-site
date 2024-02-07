d52e50b34a165d414b5d9fce70f9f1c6329014f8
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-07 21:48:26 +1000

    improve navbar logged in

--- app/_layout/navbar.tsx
+++ app/_layout/navbar.tsx
@@ -1,6 +1,35 @@
 import Link from 'next/link';
 import { auth } from '../_util/auth/auth';
 
+const loginNav = [
+  {
+    title: 'Devices',
+    items: [
+      {
+        title: 'Register New',
+        link: '/devices/register'
+      },
+      {
+        title: 'View All',
+        link: '/devices/view/all'
+      },
+    ]
+  },
+  {
+    title: 'Account',
+    items: [
+      {
+        title: 'Options',
+        link: '/user/options'
+      },
+      {
+        title: 'Sign Out',
+        link: '/api/auth/signout'
+      },
+    ]
+  }
+]
+
 export default async function Navbar() {
   const session = await auth();
 
@@ -16,10 +45,23 @@ export default async function Navbar() {
           {
             !session ? (
               <Link href='/api/auth/signin'>
-              <button className="btn">Sign In</button>
+                <button className="btn">Sign In</button>
               </Link>
             ) : (
-              <pre>{'Welcome '+session.user.name}</pre>
+              <div>
+                {loginNav.map((header, headerIndex) => (
+                  <div className="dropdown dropdown-hover dropdown-end" key={headerIndex}>
+                    <div tabIndex={0} role="button" className="btn m-1 bg-primary btn-ghost">{header.title}</div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-primary rounded-box w-52">
+                      {header.items.map((item, itemIndex) => (
+                        <li key={itemIndex} className=''>
+                          <Link href={item.link}>{item.title}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
             )
           }
         </div>
