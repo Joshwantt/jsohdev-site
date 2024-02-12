97065a60c93735b5a69afee9424c8510d98273c9
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-12 20:04:46 +1000

    device registration base

--- app/devices/register/page.tsx
+++ app/devices/register/page.tsx
@@ -1,9 +1,14 @@
 import ContentSection from "../../_components/contentSection"
 
-export default function ViewID() {
+export default function RegisterDevice() {
   return (
-    <div id="home" className="text-base-content">
-      Register Device
+    <div id="home" className="text-base-content container">
+      <div className="text-center text-4xl">Device Registration</div>
+      <div className="py-8">
+        <form>
+
+        </form>
+      </div>
     </div>
   );
 }
