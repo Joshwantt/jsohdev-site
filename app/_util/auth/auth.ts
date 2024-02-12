d9c29499b10d11dcec8547f3fab1e058bf86c659
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-12 20:04:26 +1000

    add email login option

--- app/_util/auth/auth.ts
+++ app/_util/auth/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Email from "next-auth/providers/email"
 import { DynamoDBAdapter } from "@auth/dynamodb-adapter"
 import { DynamoDB } from "@aws-sdk/client-dynamodb"
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
@@ -30,7 +31,18 @@ export const {
         }
       },
     providers: [
-        GitHub
+        GitHub,
+        Email({ //nodemailer has a bad name in the default login screen. will swap when i make custom pages for next-auth.
+          server: {
+            host: process.env.AUTH_EMAIL_SERVER_HOST,
+            port: process.env.AUTH_EMAIL_SERVER_PORT,
+            auth: {
+              user: process.env.AUTH_EMAIL_SERVER_USER,
+              pass: process.env.AUTH_EMAIL_SERVER_PASSWORD
+            }
+          },
+          from: process.env.AUTH_EMAIL_FROM
+        }),
     ],
     adapter: DynamoDBAdapter(client, {
         tableName: Table.UserTable.tableName,
