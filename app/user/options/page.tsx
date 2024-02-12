24892c42425299e842bc51b98d6c5c7df4f64239
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-02-12 17:13:36 +1000

    add placeholder options page

---
+++ app/user/options/page.tsx
@@ -0,0 +1,9 @@
+import ContentSection from "../../_components/contentSection";
+
+export default function UserOptions() {
+  return (
+    <div id="device_reg" className="text-base-content container">
+      user options placeholder
+    </div>
+  );
+}
