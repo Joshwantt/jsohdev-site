28b69f456709a9e866f573700676ef09b547949b
jsoh <41724625+Joshwantt@users.noreply.github.com>
2024-07-29 18:24:06 +1000

    remove email auth. sso only

--- app/_util/auth/auth.ts
+++ app/_util/auth/auth.ts
@@ -1,12 +1,11 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import Email from "next-auth/providers/email"
 import { DynamoDBAdapter } from "@auth/dynamodb-adapter"
 import { DynamoDB } from "@aws-sdk/client-dynamodb"
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
 import { Table } from "sst/node/table";
 
-const client = DynamoDBDocument.from(new DynamoDB(), { //maybeneed to be a new documentclient
+const client = DynamoDBDocument.from(new DynamoDB(), { 
     marshallOptions: {
         convertEmptyValues: true,
         removeUndefinedValues: true,
@@ -32,17 +31,6 @@ export const {
       },
     providers: [
         GitHub,
-        Email({ //nodemailer has a bad name in the default login screen. will swap when i make custom pages for next-auth.
-          server: {
-            host: process.env.AUTH_EMAIL_SERVER_HOST,
-            port: process.env.AUTH_EMAIL_SERVER_PORT,
-            auth: {
-              user: process.env.AUTH_EMAIL_SERVER_USER,
-              pass: process.env.AUTH_EMAIL_SERVER_PASSWORD
-            }
-          },
-          from: process.env.AUTH_EMAIL_FROM
-        }),
     ],
     adapter: DynamoDBAdapter(client, {
         tableName: Table.UserTable.tableName,
